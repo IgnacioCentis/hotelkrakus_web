d581c23d3d6879df2e900c5fb2ad10e09e7f8047
unknown <centis.ignacio@gmail.com>
2022-05-09 09:18:33 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,360 @@
+<!DOCTYPE HTML>
+<!--
+	Miniport by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Hotel Krakus - San Bernardo</title>
+		<link rel="icon" type="image/png" href="logo_krakus.png" />
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="css/jquery-ui.css">
+
+	</head>
+	<body class="is-preload">
+
+<!--
+	<div id="dialog" title="Promo hasta el !!">
+ 	<p style="margin-top: 5%;    font-family: system-ui;">  <br> 
+					  Hotel Krakus esta adherido al programa PreViaje, no dejes pasar esta oportunidad.<a href="#contact"> Envianos tu consulta!</a><br>
+					 <a href="https://previaje.gob.ar" target="blanck" style="    color: #4695da;" title="conoce mas..."> previaje.gob.ar/</a></p>
+  </div>
+-->
+
+		<div id="WAButton" style="z-index: 100;  "></div> 
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="container">
+					<li><a href="#top">Inicio</a></li>					
+					<li><a href="#habitaciones">Habitaciones</a></li>
+					<li><a href="#servicios">Servicios</a></li>
+					<li><a href="#covid">Higiene</a></li>
+					<li><a href="#contact">Consultanos</a></li>
+				</ul>
+			</nav>
+ 
+		<!-- Home -->
+			<article id="top" class="wrapper style1">
+				<div class="container">
+					<div class="row  ">
+						<div class="col-4 col-5-large col-12-medium">
+							 <img   style="margin-top: 8%; width: 85%;box-shadow:0px 0px 47px 5px #a3a3a5a1;border-radius: 4px;" src="images/frente.jpg" alt="" /> 
+						</div>
+						<div class="col-8 col-7-large col-12-medium">
+							<header>
+								<h1><img style="width: 6%;" src="images/logoHK.png" alt="" /> Hotel<strong>Krakus</strong></h1>
+							</header>
+							<p style="font-size: large;">A una cuadra del mar en la zona más tranquila del centro comercial de San Bernardo, encontrará el lugar ideal para pasar sus vacaciones junto al mar, el sol y las estrellas. Atendido por sus dueños en un confortable ambiente familiar. Sus colaboradores están preparados para brindar un servicio de calidad, que harán inolvidable su estadía.</p>
+							<!--<a href="#habitaciones" class="button large scrolly">Recorrer el Hotel</a>-->
+						</div>
+					</div>
+				</div>
+			</article>
+
+		<!-- HABITACIONES -->
+			<article id="habitaciones" class="wrapper style3">
+				<div class="container">
+					<header>
+						<h2>Nuestras Habitaciones</h2>
+						<p style="font-size: revert;">Las habitaciones están íntegramente alfombradas y climatizadas. Cuentan con:<br>
+								TV LCD con cable</br>
+								Camas Sommiers</br>
+								Frigobar</br>
+								Baño privado con ducha sectorizada y secadores de cabello</br>
+								Cofre de Seguridad Individual</br>
+								Teléfono con acceso a DDN y DDI con tarifador automático</br>
+								Servicio de Mucama y Lavandería</br>
+
+							  </p>
+					</header>
+					<div class="row">
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+
+								<a  class="image featured"><img src="images/HAB9.jpg" alt="" /></a>
+								 
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/HAB3.jpg" alt="" /></a>
+								 
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/HAB4.jpg" alt="" /></a>
+								 
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/HAB5.jpg" alt="" /></a>
+								 
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a   class="image featured"><img src="images/HAB6.jpg" alt="" /></a>
+								 
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a   class="image featured"><img src="images/HAB7.jpg" alt="" /></a>
+								 
+							</article>
+						</div>
+					</div>
+					<footer>
+						 
+						<a href="#servicios" class="button scrolly">Nuestros Servicios</a>
+					</footer>
+				</div>
+			</article>
+
+			<!-- SERVICIOS -->
+			<article id="servicios" class="wrapper style3">
+				<div class="container">
+					<header>
+						<h2>Servicios</h2>
+					</header>
+					<div class="row">
+																
+						
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a   class="image featured"><img src="images/SERV5.jpg" alt="" /></a>
+								<h3><a >Solarium</a></h3>
+								<p>Viento o frio en la playa, no importa podrás disfrutar de nuestro Solarium disponible las 24 hs</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/SERV2.jpg" alt="" /></a>
+								<h3><a >Estacionamiento</a></h3>
+								<p>Propio dentro del establecimiento, con vigilacia (solo para autos)</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/SERV7.jpg" alt="" /></a>
+								<h3><a >Comedor</a></h3>
+								<p> Comedor con vajilla completa y microondas disponible de 13.00 a 00.00 hs de libre uso</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/SERV3.jpg" alt="" /></a>
+								<h3><a >Artículos para Playa	</a></h3>
+								<p>Sillitas, Toallas, Lonas y Juegos para llevar a la playa</p>
+							</article>
+						</div>	
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a   class="image featured"><img src="images/SERV9.jpg" alt="" /></a>
+								<h3><a >Sala de Estar</a></h3>
+								<p>Amplia sala de estar de uso libre</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a   class="image featured"><img src="images/SERV6.jpg" alt="" /></a>
+								<h3><a >Entretenimiento</a></h3>
+							<p>Sala de estar con computadora, videojuegos Arcade, Metegol, juegos de mesas, TV y más..</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/SERV4.jpg" alt="" /></a>
+								<h3><a >Desayuno Buffet</a></h3>
+								<p>Rico y variado desayuno continental buffet. (no disponible temporada 2021)</p>
+							</article>
+						</div>		
+						
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a   class="image featured"><img src="images/SERV12.jpg" alt="" /></a>
+								<h3><a >Desayuno a la Habitacion</a></h3>
+								<p> Para cuidarte y cuidarnos, esta temporada el desayuno es a la Habitaciona.</br> Mas comodidad, igual calidad.</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/SERV1.jpg" alt="" /></a>
+								<h3><a >Desayuno</a></h3>
+								<p>Variado desayuno a la habitación cumpliendo los protocolos de salud vigentes</p>
+							</article>
+						</div>
+					</div>
+					<footer>
+						 
+						<a href="#covid" class="button  scrolly">Protocolos de Salud (COVID-19)</a>
+					</footer>
+				</div>
+			</article>
+
+			<!-- Higiene -->
+			<article id="covid" class="wrapper style2" style="padding-top: 40px;padding-bottom: 40px;">
+				<div class="container">
+					<header>
+						<h2>Cumplimos con los Protocolos de Salud Covid-19</h2>
+						<p>Cuidándonos entre todos...</p>
+					</header>
+					<div class="row">
+						<div class="col-4 col-6-medium col-12-small "> 
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/COVID1.jpg" alt="" /></a>
+								<h3><a >Sanitizantes con Alcohol al 70%</a></h3>
+								<p>Distribuimos distintos tipos de sanitizantes en puntos claves del Hotel</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a  class="image featured"><img src="images/COVID2.jpg" alt="" /></a>
+								<h3><a >Medidas de Seguridad</a></h3>
+								<p>Al ingreso al establecimiento deberás firmar la DJ de Salud, llevaremos un control de temperatura y siempre deberán respetar en los espacios comunes el distancionamieno social y utilizar barbijos</p>
+							</article>
+						</div>
+						<div class="col-4 col-6-medium col-12-small">
+							<article class="box style2 sombra agranda">
+								<a href="politicas_krakus.pdf" class="image featured"><img src="images/COVID3.jpg" alt="" /></a>
+								<h3><a >Protocolos y Recomendaciones Covid-19</a></h3>
+								<p>Hace clic en la imagen y enterate de los protocolos Covid-19 y Recomendaciones </p>
+							</article>
+						</div>
+					</div>
+ 	
+					<footer>
+						 
+						<a href="#contact" class="button  scrolly">Envíenos tu Consulta</a>
+					</footer>
+				</div>
+			</article>
+
+		<!-- Contact -->
+			<article id="contact" class="wrapper style3">
+				<div class="container medium">
+					<header>
+						<h2>Consultanos</h2>
+						<p>Realiza tu consulta y en breve nos comunicaremos</p>
+					</header>
+					<div class="row">
+						<div class="col-12">
+							<form id="formConsulta" method="post" action="envioConsulta.php">
+								<div class="row">
+									<div class="col-6 col-12-small">
+										<input type="text" name="name" id="name" placeholder="Nombre y Apellido"  required/>
+									</div>
+									<div class="col-6 col-12-small">
+										<input type="text" name="email" id="email" placeholder="Email"  required/>
+									</div>
+									<div class="col-12 col-12-small">
+										<input type="text" name="telefono" id="telefono" placeholder="telefono"  required/>
+									</div>
+									<div class="col-6 col-6-small">
+										<input type="text" class="datepicker2" name="checkin" id="checkin" placeholder="Check In" />
+									</div>
+									<div class="col-6 col-6-small">
+										<input type="text" class="datepicker2" name="checkout" id="checkout" placeholder="Check Out" />
+									</div>
+									<div class="col-6 col-6-small">
+										<input type="text" name="adultos" id="adultos" placeholder="Cant. Adultos" />
+									</div>
+									<div class="col-6 col-6-small">
+										<input type="text" name="menores" id="menores" placeholder="Cant. Menores" />
+									</div>
+									<div class="col-12">
+										<textarea name="message" id="message" placeholder="Mensaje" required></textarea>
+									</div>
+									<div class="col-12">
+										<ul class="actions">
+											<li><input id="enviar" type="submit" value="Enviar Consulta" /></li>
+											<li><input type="reset" value="Limpiar Formulario" class="alt" /></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+						</div>
+						<div class="col-12">
+							<hr />
+
+							<div class="col-4 col-6-medium col-12-small">					
+									<iframe class="image featured" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3198.91544634562!2d-56.679294685117725!3d-36.70056967893627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959c6f168de875b5%3A0x87af069117ea7858!2sHOTEL%20KRAKUS!5e0!3m2!1ses!2sar!4v1632343100154!5m2!1ses!2sar" width="700" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+									<h3>Seguinos en..</h3>
+							<ul class="social">						
+								<li><a href="https://www.facebook.com/KrakusHotel/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.instagram.com/krakushotel/" class="icon brands fa-instagram"><span  class="label">Instagram</span></a></li>		
+							</ul>
+								
+							</div>
+							<div class="col-4 col-6-medium col-12-small">
+								<hr /> 
+							</div>
+							
+						</div>
+					</div>
+		
+					<footer>					 
+						<ul id="copyright">
+							<li> Chiozza 1645 - San Bernardo - Partido de La Costa - Buenos Aires</li>
+							<li> (02257) 460438 / 467186</li>
+							<li> krakushotel@gmail.com</li>
+							<li>&copy; Hotel Krakus. Todos los derechos reservados 2020.</li> 
+						</ul>
+					</footer>
+				</div>
+			</article>
+
+		<!-- Scripts -->
+		  <!--  --> 
+		  	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  			<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			<script src="js/jquery.min.js"></script>
+  			<script src="js/jquery-ui.js"></script> -->
+			<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+			<!--Floating WhatsApp css-->  
+			<link rel="stylesheet" href="floating_whatsapp/floating-wpp.min.css">  
+			<!--Floating WhatsApp javascript-->  
+			<script type="text/javascript" src="floating_whatsapp/floating-wpp.min.js"></script>
+
+	</body>
+</html>
+
+<script type="text/javascript">  
+
+  $(document).ready(function(){
+
+/*
+	  $( function() {
+	    $( "#dialog" ).dialog();
+	  } );
+*/
+  	$(function () {
+           $('#WAButton').floatingWhatsApp({
+               phone: '5492257552794', //WhatsApp Business phone number
+               headerTitle: 'Realízanos una Consulta!', //Popup Title
+               popupMessage: 'Hola, En que te podemos ayudar?', //Popup Message
+               message: 'Hola, me contacto desde tu sitio web: ',
+               showPopup: true, //Enables popup display
+               buttonImage: '<img src="floating_whatsapp/whatsapp.svg" />', //Button Image
+               size:'50px',
+               //headerColor: 'crimson', //Custom header color
+               //backgroundColor: 'crimson', //Custom background button color
+               position: "right" //Position: left | right
+
+           });
+       });
+  })
+	
+   
+</script>  
